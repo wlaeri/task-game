cd838a8c3d05771c7cb77eac80fbeb3f01825009
Chris DeMoll <demoll.chris@gmail.com>
2016-08-15 19:40:29 -0400

    revised seed and gameFactory

---
+++ browser/js/game-overview/game-factory.js
@@ -0,0 +1,22 @@
+'use strict';
+
+app.factory('GameFactory', function($http) {
+  var GameFactory = {};
+
+  GameFactory.getGame = function(id) {
+    return $http.get('api/game/' + id)
+    .then(game => game.data);
+  };
+
+  GameFactory.createGame = function(data) {
+    return $http.post('api/game/', data)
+    .then(newGame => newGame.data);
+  }
+
+  GameFactory.createTask = function(data) {
+    return $http.post('api/events', data)
+    .then(newTask => newTask.data);
+  }
+
+  return GameFactory;
+})
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,8 +2,8 @@
 var router = require('express').Router();
 module.exports = router;
 
-router.use('/members', require('./members'));
-
+router.use('/game', require('./games.router'));
+router.use('/events', require('./events.router'));
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
