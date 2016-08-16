a113685167a3ec5f5002a7efcad588f95d9b389e
Will Laeri <wlaeri@gmail.com>
2016-08-16 16:42:55 -0400

    login/signup transitions to u state

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,9 +1,16 @@
 app.controller('HomeCtrl', function($scope, $mdDialog){
     $scope.login = function() {
-        console.log("login function");
         $mdDialog.show({
             templateUrl: 'js/login/login.html',
             controller: 'LoginCtrl'
         });
     }
+
+    $scope.signup = function() {
+        $mdDialog.show({
+            templateUrl: 'js/signup/signup.html',
+            controller: 'SignupCtrl'
+        });
+    }
+
 })
