51906a2ff9d57a4c123072f4860954f859273d1a
Will Laeri <wlaeri@gmail.com>
2016-08-17 13:34:09 -0400

    create games functionality continued...

--- browser/js/add-friends/add-friends.controller.js
+++ browser/js/add-friends/add-friends.controller.js
@@ -1,5 +1,28 @@
-app.controller('AddFriendsCtrl', function($scope, $mdDialog){
+app.controller('AddFriendsCtrl', function($scope, $state, $mdDialog){
 
-    $scope.friends = [];
+    console.log('$scope.friends', $scope.friends);
+
+    $scope.addFriend = function(email) {
+        let newFriendNum = $scope.friends.length + 1;
+        $scope.friends.push({
+            id:'friend' + newFriendNum,
+            email: email
+        });
+    };
+
+    $scope.removeFriend = function(friendId) {
+        if(friendId === 'friend1') return;
+        $scope.friends = $scope.friends.filter(e=>e.id !== friendId);
+    };
+
+    $scope.handleSubmit = function () {
+        //$state.go('u.create', {players: $scope.friends});
+        return $mdDialog.hide();
+    }
+
+    $scope.handleCancel = function () {
+        //$state.go('u.create');
+        return $mdDialog.hide();
+    };
 
 })
