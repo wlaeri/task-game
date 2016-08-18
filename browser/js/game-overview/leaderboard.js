4556a9f45cc6dc41d48967e2bc6bb399221ecee4
Barry Weinberg <bjw2119@gmail.com>
2016-08-18 11:41:29 -0400

    Work on leaderboard

--- browser/js/game-overview/leaderboard.js
+++ browser/js/game-overview/leaderboard.js
@@ -4,15 +4,32 @@ app.directive('tgLeaderboard', function(){
     templateUrl: 'js/game-overview/leaderboard.html',
     scope: {
       events: '=',
-      players: '='
+      players: '=',
+      tasks: '='
     },
     link: function(scope){
-      scope.players.forEach(p=>{
-        var total = 0;
-        var foundEvents = scope.events.filter(function(e){return e.completedById == p.id})
-        console.log(foundEvents);
-        if(foundEvents[0]) p.points = foundEvents.reduce(function(total, e){return total+parseInt(e[points])});
-        else p.points =0;
-      })
+      console.log(scope.events);
+      scope.events.forEach(e=>{
+        var foundTask = scope.tasks.find(function(task){return task.id == e.taskId})
+        // e.task = foundTask.name;
+        console.log("Found Task:  ", foundTask);
+        e.points = foundTask.points;
+        console.log("Found TP: ", foundTask.points);
+        console.log("E points", e.points);
+        scope.players.find(player=>player.id == e.completedById).points+= parseInt(e.points);
+        console.log(scope.players.find(player=>player.id == e.completedById));
+      });
     }
 }});
+      // scope.players.forEach((p, i)=>{
+      //   var total = 0;
+      //   var foundEvents = scope.events.filter(function(e){return e.completedById == p.id})
+
+      //   console.log(foundEvents);
+      //   console.log(foundEvents[0]);
+      //   if(foundEvents[0]) scope.players[i].points = foundEvents.reduce(function(prev, curr){
+      //     console.log("Curr is: ", curr);
+      //     console.log(curr.points);
+      //     return prev + parseInt(curr.points)}, 0);
+      //   else scope.players[i].points =0;
+      // })
