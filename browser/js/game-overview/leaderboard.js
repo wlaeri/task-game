c3751a41ddc10115f891e7814f0bf0c7db7573af
Chris DeMoll <demoll.chris@gmail.com>
2016-08-18 12:15:26 -0400

    full functionality in leaderboard

--- browser/js/game-overview/leaderboard.js
+++ browser/js/game-overview/leaderboard.js
@@ -8,17 +8,33 @@ app.directive('tgLeaderboard', function(){
       tasks: '='
     },
     link: function(scope){
-      console.log(scope.events);
-      scope.events.forEach(e=>{
-        var foundTask = scope.tasks.find(function(task){return task.id == e.taskId})
-        // e.task = foundTask.name;
-        console.log("Found Task:  ", foundTask);
-        e.points = foundTask.points;
-        console.log("Found TP: ", foundTask.points);
-        console.log("E points", e.points);
-        scope.players.find(player=>player.id == e.completedById).points+= parseInt(e.points);
-        console.log(scope.players.find(player=>player.id == e.completedById));
+      scope.players = scope.players.map(function(player, i) {
+        var playerPoints = scope.events.filter(function(event) {
+          return event.completedById === player.id;
+        }).map(function(event) {
+          var eventTask = scope.tasks.filter(function(task) {
+            return task.id === event.taskId;
+          });
+          return eventTask[0].points;
+        });
+        scope.players[i].points = playerPoints.reduce(function(prev, curr) {
+          return prev + curr;
+        }, 0);
+        return scope.players[i];
+      }).sort(function(a,b) {
+        return b.points - a.points;
       });
+      // console.log(scope.events);
+      // scope.events.forEach(e=>{
+      //   var foundTask = scope.tasks.find(function(task){return task.id == e.taskId})
+      //   // e.task = foundTask.name;
+      //   console.log("Found Task:  ", foundTask);
+      //   e.points = foundTask.points;
+      //   console.log("Found TP: ", foundTask.points);
+      //   console.log("E points", e.points);
+      //   scope.players.find(player=>player.id == e.completedById).points+= parseInt(e.points);
+      //   console.log(scope.players.find(player=>player.id == e.completedById));
+      // });
     }
 }});
       // scope.players.forEach((p, i)=>{
