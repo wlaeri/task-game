9be71077413e0761c7a0988f0decd076836d4da5
Will Laeri <wlaeri@gmail.com>
2016-08-19 14:21:38 -0400

    datetimepicker working

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,5 +1,13 @@
 'use strict';
-window.app = angular.module('Gamr', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'ngMaterial', 'ngAria']);
+window.app = angular.module('Gamr', [
+    'fsaPreBuilt',
+    'ui.router',
+    'ui.bootstrap',
+    'ngAnimate',
+    'ngMaterial',
+    'ngAria',
+    'ngMaterialDatePicker'
+]);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
