17da30dd3c66300a4c6d7348d5a6768b336dd09a
bjw2119 <bjw2119@users.noreply.github.com>
2016-08-18 20:55:50 -0400

    Add default value to be 'pending'

--- server/db/models/game.model.js
+++ server/db/models/game.model.js
@@ -24,11 +24,9 @@ module.exports = db.define('game', {
         type: Sequelize.BOOLEAN,
         defaultValue: false
     },
-    invitedPlayers: {
-        type: Sequelize.ARRAY(Sequelize.INTEGER)
-    },
     status: {
         type: Sequelize.ENUM('Pending', 'Confirmed', 'Active', 'Complete'),
+        defaultValue: 'Pending',
         get: function() {
             var now = new Date();
             return this.start < now && !this.locked ? 'Pending' : this.start < now && this.locked ? 'Confirmed' : this.end < now ? 'Complete' : 'Active';
