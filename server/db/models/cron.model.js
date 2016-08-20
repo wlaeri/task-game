b870b2a0af168d808a19742a8929bf3c58f1faed
John Henry <John.J.Henry4@gmail.com>
2016-08-20 13:03:22 -0400

    cron tools in place

--- server/db/models/cron.model.js
+++ server/db/models/cron.model.js
@@ -3,7 +3,7 @@
 let db = require('../_db');
 var Sequelize = require('sequelize');
 
-module.exports = db.define('cron', {
+var Cron = db.define('cron', {
     startDate: {
         type: Sequelize.DATE,
         allowNull: false
@@ -11,5 +11,28 @@ module.exports = db.define('cron', {
     endDate: {
         type: Sequelize.DATE,
         allowNull: false
+    },
+    startDay: {
+    	type: Sequelize.STRING
     }
+}, { 
+hooks: {
+	beforeCreate: function(cron){
+        cron.startDay = "";
+		console.log(cron.startDate);
+		var date = new Date(cron.startDate);
+		var year = date.getYear().toString();
+		var month = date.getMonth().toString();
+		var day = date.getDay().toString();
+        var formattedDate = day+"/"+month+"/"+year; 
+        var testDate = new Date(formattedDate);
+		console.log("formatted Date", testDate);
+
+
+		cron.startDay = testDate; 
+	}
+
+}
 });
+
+module.exports = Cron;
