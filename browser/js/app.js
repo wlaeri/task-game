6fe1c90c19d5011b6e1bc4eb03caaef0c0d08b8b
Barry Weinberg <bjw2119@gmail.com>
2016-08-22 16:32:32 -0400

    Move user state styling to scss

--- browser/js/app.js
+++ browser/js/app.js
@@ -21,6 +21,97 @@ app.config(function ($urlRouterProvider, $locationProvider) {
 
 });
 
+app.config(function($mdThemingProvider){
+    var customPrimary = {
+        '50': '#baedf3',
+        '100': '#a4e7f0',
+        '200': '#8ee1ec',
+        '300': '#79dce8',
+        '400': '#63d6e5',
+        '500': '#4DD0E1',
+        '600': '#00B5CD',
+        '700': '#24c2d7',
+        '800': '#21afc1',
+        '900': '#1d9bab',
+        'A100': '#d0f3f7',
+        'A200': '#e6f8fb',
+        'A400': '#fcfefe',
+        'A700': '#198795'
+    };
+    $mdThemingProvider
+        .definePalette('GamrPrimary',
+                        customPrimary);
+
+    var customAccent = {
+        '50': '#ffdf61',
+        '100': '#ffe47a',
+        '200': '#ffea94',
+        '300': '#ffefad',
+        '400': '#fff4c7',
+        '500': '#fff9e0',
+        '600': '#ffffff',
+        '700': '#ffffff',
+        '800': '#ffffff',
+        '900': '#ffffff',
+        'A100': '#ffffff',
+        'A200': '#FFFEFA',
+        'A400': '#fff9e0',
+        'A700': '#ffffff'
+    };
+    $mdThemingProvider
+        .definePalette('GamrAccent',
+                        customAccent);
+
+    var customWarn = {
+        '50': '#ffb280',
+        '100': '#ffa266',
+        '200': '#ff934d',
+        '300': '#ff8333',
+        '400': '#ff741a',
+        '500': '#ff6400',
+        '600': '#e65a00',
+        '700': '#cc5000',
+        '800': '#b34600',
+        '900': '#993c00',
+        'A100': '#ffc199',
+        'A200': '#ffd1b3',
+        'A400': '#ffe0cc',
+        'A700': '#803200'
+    };
+    $mdThemingProvider
+        .definePalette('GamrWarn',
+                        customWarn);
+
+    var customBackground = {
+        '50': '#ffffff',
+        '100': '#ffffff',
+        '200': '#ffffff',
+        '300': '#ffffff',
+        '400': '#ffffff',
+        '500': '#FFFEFA',
+        '600': '#fff9e0',
+        '700': '#fff4c7',
+        '800': '#ffefad',
+        '900': '#ffea94',
+        'A100': '#ffffff',
+        'A200': '#ffffff',
+        'A400': '#ffffff',
+        'A700': '#ffe47a'
+    };
+    $mdThemingProvider
+        .definePalette('GamrBackground',
+                        customBackground);
+
+   $mdThemingProvider.theme('default')
+       .primaryPalette('GamrPrimary', {
+        'default': '500',
+        'hue-2': '600'
+       })
+       .accentPalette('GamrAccent')
+       .warnPalette('GamrWarn')
+       .backgroundPalette('GamrBackground')
+});
+
 // This app.run is for controlling access to specific states.
 app.run(function ($rootScope, AuthService, $state) {
 
