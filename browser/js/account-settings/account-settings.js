166ba62d89c5e75f2bf66c64f8a7ddc0ea709a4d
John Henry <John.J.Henry4@gmail.com>
2016-08-23 21:52:27 -0400

    working account apge

--- browser/js/account-settings/account-settings.js
+++ browser/js/account-settings/account-settings.js
@@ -6,6 +6,55 @@ app.config(function($stateProvider){
   });
 });
 
-app.controller('AccountSettingsCtrl', function($scope){
-  $scope.hello = "hello"+$scope.user.email;
+app.controller('AccountSettingsCtrl', function($scope, UserFactory){
+
+  $scope.updatePassword = function(){
+  	UserFactory.updateUser($scope.user.id, {password: $scope.password1});
+}
+
+  $scope.newEmail = '';
+
+  $scope.updateEmail = function(){
+    console.log($scope.newEmail);
+    UserFactory.updateUser($scope.user.id, {email: $scope.newEmail})
+  }
+
+  $scope.displayEmailChange = false;
+  $scope.displayPasswordChange = false; 
+
+  $scope.password = {password1: '', password2: ''};
+
+  $scope.passwordsMatch = false
+
+  $scope.matching = function(){
+  //   var password1 = document.getElementById("passwordField1").value;
+  //   var password2 = document.getElementById("passwordField2").value
+  //   console.log('password1', password1, 'password2', password2);
+  //   if (password1 === password2 && password1.length>6){
+  //      $scope.passwordsMatch = true;
+  //   }
+  //   else{
+  //     $scope.passwordsMatch = false;
+  //   }
+  // }
+
+  	if ($scope.password.password1 === $scope.password.password2 && $scope.password.password1.length>6){
+  		 $scope.passwordsMatch = true;
+  	}
+    else{
+      $scope.passwordsMatch = false;
+    }
+  }
+
+  $scope.openPasswordChange = function(){
+  	$scope.displayPasswordChange = !$scope.displayPasswordChange;
+  }
+  $scope.openEmailChange = function(){
+    $scope.displayEmailChange = !$scope.displayEmailChange;
+  }
+
+  $( "input" ).on('keyup', function() {
+  $scope.matching();
+});
+
 });
